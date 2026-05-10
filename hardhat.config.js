dd6aca8c80c56de4883f1ffe5561ebafa864ad52
Ivan Oliinyk <xoliinyki1@stuba.sk>
2026-05-10 13:11:52 +0200

    update contract address

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,32 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 require("solidity-coverage");
+require("@nomicfoundation/hardhat-verify");
 
 module.exports = {
   solidity: "0.8.24",
+
   networks: {
     opSepolia: {
       url: "https://sepolia.optimism.io",
       accounts: [process.env.PRIVATE_KEY]
     }
+  },
+
+  etherscan: {
+    apiKey: {
+      optimisticSepolia: "abc"
+    },
+
+    customChains: [
+      {
+        network: "optimisticSepolia",
+        chainId: 11155420,
+        urls: {
+          apiURL: "https://optimism-sepolia.blockscout.com/api",
+          browserURL: "https://optimism-sepolia.blockscout.com"
+        }
+      }
+    ]
   }
 };
